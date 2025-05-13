95c45d3b59eb34b8b030bc5ae84a40b348124533
vladfeitser <vlad.feitser@mobius.team>
2025-05-13 21:30:58 +0300

    add tabs and new pages

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { View } from "react-native";
+import { styles } from '../../styles/auth.styles';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href={"/notifications"}>Visit notifications screen</Link>
+    </View>
+  );
+}
+
